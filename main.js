5e885e6744aaf643080ada0ca415f2a8311f5172
osmangonigithub <osmangone939@gmail.com>
2020-08-16 10:27:25 +0600

    issue fix

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
-
+var totalIssue=0;
+var openIssue=0;
 function submitIssue(e) {
   const getInputValue = id => document.getElementById(id).value;
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
   const id = Math.floor(Math.random()*100000000) + '';
+  const openProblem= document.getElementById('open-issue').innerText;
+  const totalProblem= document.getElementById('total-issue').innerText;
   const status = 'Open';
 
-  const issue = { id, description, severity, assignedTo, status };
+  const issue = { id, description, severity, assignedTo, status, openProblem, totalProblem};
   let issues = [];
   if (localStorage.getItem('issues')){
     issues = JSON.parse(localStorage.getItem('issues'));
@@ -19,20 +22,30 @@ function submitIssue(e) {
   document.getElementById('issueInputForm').reset();
   fetchIssues();
   e.preventDefault();
+  totalIssue++;
+  openIssue++;
+  document.getElementById('total-issue').innerText=totalIssue;
+  document.getElementById('open-issue').innerText=openIssue;
 }
 
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  const currentIssue = issues.find(issue => issue.id == id);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
+  document.getElementById('open-issue').innerText=openIssue;
 }
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter(issue=>issue.id != id )
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
+  totalIssue--;
+  document.getElementById('total-issue').innerText=totalIssue;
+  openIssue--;
+  document.getElementById('open-issue').innerText=openIssue;
 }
 
 const fetchIssues = () => {
@@ -41,7 +54,8 @@ const fetchIssues = () => {
   issuesList.innerHTML = '';
 
   for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
+    const {id, description, severity, assignedTo, status} = issues[i]
+    console.log(id)
 
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
@@ -49,8 +63,10 @@ const fetchIssues = () => {
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" id="${id}" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
+
+
 }
