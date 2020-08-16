2e778bd6d4f462453494c6f56a059834dd4aa77d
osmangonigithub <osmangone939@gmail.com>
2020-08-16 17:48:33 +0600

    almost done

--- main.js
+++ main.js
@@ -1,40 +1,51 @@
 document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
-var totalIssue=0;
-var openIssue=0;
+const totalIssue=JSON.parse(localStorage.getItem('issues'));
+const closedIssue=totalIssue.filter(issue=>issue.status=="Closed");
+document.getElementById('open-issue').innerText=totalIssue.length- closedIssue.length;
 function submitIssue(e) {
+  if(document.getElementById('issueDescription').value.length<1){
+    alert('Description is required');
+    return;
+  }
+  if(document.getElementById('issueAssignedTo').value.length<1){
+    alert('your name is required');
+    return;
+  }
   const getInputValue = id => document.getElementById(id).value;
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
   const id = Math.floor(Math.random()*100000000) + '';
-  const openProblem= document.getElementById('open-issue').innerText;
-  const totalProblem= document.getElementById('total-issue').innerText;
   const status = 'Open';
-
-  const issue = { id, description, severity, assignedTo, status, openProblem, totalProblem};
   let issues = [];
+  const issue = { id, description, severity, assignedTo, status};
+  
   if (localStorage.getItem('issues')){
     issues = JSON.parse(localStorage.getItem('issues'));
   }
+  console.log(issues);
   issues.push(issue);
   localStorage.setItem('issues', JSON.stringify(issues));
 
   document.getElementById('issueInputForm').reset();
   fetchIssues();
   e.preventDefault();
-  totalIssue++;
-  openIssue++;
-  document.getElementById('total-issue').innerText=totalIssue;
-  document.getElementById('open-issue').innerText=openIssue;
+  let tIssue=JSON.parse(localStorage.getItem('issues'));
+  const cIssue=tIssue.filter(issue=>issue.status=="Closed");
+  document.getElementById('total-issue').innerText=tIssue.length;
+  document.getElementById('open-issue').innerText=tIssue.length- cIssue.length;
 }
-
+var totalIssueLength=0;
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const currentIssue = issues.find(issue => issue.id == id);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
-  document.getElementById('open-issue').innerText=openIssue;
+  let tIssue=JSON.parse(localStorage.getItem('issues'));
+  const cIssue=tIssue.filter(issue=>issue.status=="Closed");
+  document.getElementById('total-issue').innerText=tIssue.length;
+  document.getElementById('open-issue').innerText=tIssue.length- cIssue.length;
 }
 
 const deleteIssue = id => {
@@ -42,11 +53,14 @@ const deleteIssue = id => {
   const remainingIssues = issues.filter(issue=>issue.id != id )
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
   fetchIssues();
-  totalIssue--;
-  document.getElementById('total-issue').innerText=totalIssue;
-  openIssue--;
-  document.getElementById('open-issue').innerText=openIssue;
+  let tIssue=JSON.parse(localStorage.getItem('issues'));
+  const cIssue=tIssue.filter(issue=>issue.status=="Closed");
+  document.getElementById('total-issue').innerText=tIssue.length;
+  document.getElementById('open-issue').innerText=tIssue.length- cIssue.length;
 }
+
+console.log(totalIssue.length);
+document.getElementById('total-issue').innerText=totalIssue.length;
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
@@ -55,7 +69,6 @@ const fetchIssues = () => {
 
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i]
-    console.log(id)
 
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
@@ -67,6 +80,6 @@ const fetchIssues = () => {
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
-
-
+  
 }
+
